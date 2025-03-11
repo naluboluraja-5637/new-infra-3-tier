14019186c10d650c1164cbbf0c2afaa53cd9d507
naluboluraja-5637 <nalubolurajasekhar7386@gmail.com>
2025-03-11 11:32:48 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 # Create an S3 Bucket
-resource "aws_s3_bucket" "example_bucket" {
+resource "aws_s3_bucket" "two_bucket" {
   bucket = "swiggy-bucket-terraformraja56377386"
 
   # Enable versioning
